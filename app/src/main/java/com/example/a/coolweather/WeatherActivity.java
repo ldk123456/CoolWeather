18ddf34ad6e97e02474738ed8121999d3c0cf862
LDK <1786079189@qq.com>
2018-05-17 23:07:34 +0800

    backstage auto update weather

--- app/src/main/java/com/example/a/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/a/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.a.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.a.coolweather.GSON.Forecast;
 import com.example.a.coolweather.GSON.Weather;
+import com.example.a.coolweather.service.AutoUpdateService;
 import com.example.a.coolweather.util.HttpUtil;
 import com.example.a.coolweather.util.Utility;
 
@@ -199,6 +201,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent=new Intent(this, AutoUpdateService.class);
+        startActivity(intent);
     }
 
     /**
